42b181f09eefdc1269c0a8b5d15b4cf986b9e9ef
pabitrasahoo98 <152062702+pabitrasahoo98@users.noreply.github.com>
2024-08-24 23:50:44 +0530

    first commit1

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const cors= require("cors")
 const bodyParser=require("body-parser")
 
 const errorMiddleware=require("./middleware/error")
-app.use(cors());
+app.use(cors(
+  {
+      origin:['http://localhost:3000',],
+      methods:["GET","POST","PUT","DELETE"],
+      credentials:true,
+  }
+));
 
 app.use(express.json({limit:'50mb'}))
 app.use(bodyParser.urlencoded({extended:true,limit:"50mb"}))
