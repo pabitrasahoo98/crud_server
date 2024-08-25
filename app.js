8c33e6867c1032472ba4efc15d57085d9986c052
pabitrasahoo98 <152062702+pabitrasahoo98@users.noreply.github.com>
2024-08-25 18:45:41 +0530

    first commit2

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser=require("body-parser")
 const errorMiddleware=require("./middleware/error")
 app.use(cors(
   {
-      origin:['http://localhost:3000',],
+      origin:['http://localhost:3000','https://crud-app-three-phi.vercel.app/'],
       methods:["GET","POST","PUT","DELETE"],
       credentials:true,
   }
